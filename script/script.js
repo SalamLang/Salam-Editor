725641cad78bd4effa379fcd2b0dbc2d6d4b9077
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-11 02:37:08 +0330

    feat(data): change sent data

--- script/script.js
+++ script/script.js
@@ -244,9 +244,9 @@ elm_save.addEventListener("click", () => {
         xhr.open("POST", APP_URL + "/api/v1/verify_token");
         
         xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        xhr.send(JSON.stringify({
+        xhr.send({
             token: token
-        }));        
+        });        
     }
 })
 
