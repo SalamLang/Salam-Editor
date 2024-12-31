3d5e4e1371fb3a975ba4e216a7f3d17f47aa6af8
mohamadreza <mohamadreza1388.org@gmail.com>
2024-12-31 11:53:52 +0330

    feat(data): add sum data for auto complete

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,21 +1,32 @@
 import {useEffect} from "react";
 import "../../scripts/codemirror.js"
 import EditorService from "../../services/EditorService.js";
-import {useLocation} from "react-router-dom";
 
 const Editor = () => {
-    let location = useLocation();
-
     useEffect(() => {
-        EditorService((updateText) => {
-            // console.log(updateText)
+        EditorService(() => {
+            return [
+                {
+                    label: "صفحه",
+                    type: "keyword",
+                    apply: "صفحه" + ":\nتمام"
+                },
+                {
+                    label: "دکمه",
+                    type: "variable",
+                    apply: "دکمه" + ":\nتمام",
+                    info: "این تگ برای ایجاد یک دکمه به کار میرود.",
+                }
+            ]
+        },(updateText) => {
+
         })
-    }, [location])
+    }, [])
 
     return (<>
         <main
             className={"editor-container w-[calc(100%-50px)] h-[calc(100vh-35px)] bg-[#FFF1E9] rounded-tr-[15px] rtl after:inline-block after:border-0 after:z-[-1] after:w-[30px] after:h-[30px] float-end after:bg-[#ffdecc] after:absolute relative after:top-0 after:right-0"}>
-            <div id="editor" className={"rounded-tr-[15px] overflow-hidden"}></div>
+            <div id="editor" className={"rounded-tr-[15px] overflow-auto h-[calc(100vh-35px)]"}></div>
         </main>
     </>);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import EditorLayout from "./layouts/EditorLayout.jsx";
 import RouteChangeHandler from "./components/nprogress/RouteChangeHandler.jsx";
 
 createRoot(document.getElementById('root')).render(<>
     <BrowserRouter>
-        <RouteChangeHandler />
+        <RouteChangeHandler/>
         <Routes>
-            <Route path="/" element={ <EditorLayout /> } />
+            <Route path="/" element={<EditorLayout/>}/>
         </Routes>
     </BrowserRouter>
 </>)
