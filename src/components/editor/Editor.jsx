b86aec12904785c306fb800ab67c9f381449c697
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-01 16:54:55 +0330

    feat(error): add error and ifream output code

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -23,6 +23,9 @@ const Editor = () => {
         <main
             className={"editor-container w-[calc(100%-50px)] h-[calc(100vh-35px)] bg-[#FFF1E9] rounded-tr-[15px] rtl after:inline-block after:border-0 after:z-[-1] after:w-[30px] after:h-[30px] float-end after:bg-[#ffdecc] after:absolute relative after:top-0 after:right-0"}>
             <div id="editor" className={"rounded-tr-[15px] overflow-auto h-[calc(100vh-35px)]"}></div>
+            {/*<div className="output"></div>*/}
+            {/*<div className="error"></div>*/}
+            {/*<iframe className={"iframe"}></iframe>*/}
         </main>
     </>);
 };
