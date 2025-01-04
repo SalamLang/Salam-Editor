6d496a3b2677b9b645ba1d966816bcfa7a2ecebc
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-04 14:39:46 +0330

    feat(test)

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,7 +1,7 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import "../../scripts/codemirror.js"
 import EditorService from "../../services/EditorService.js";
-import Draggable from "react-draggable";
+import { Rnd } from "react-rnd";
 
 const salamAdd = () => {
     const script = document.createElement('script');
@@ -14,6 +14,9 @@ const salamAdd = () => {
 }
 
 const Editor = () => {
+    const [size, setSize] = useState({ width: 300, height: 200 });
+    const [position, setPosition] = useState({ x: 50, y: 50 });
+
     useEffect(() => {
         // salamAdd();
         EditorService(() => {
@@ -35,9 +38,21 @@ const Editor = () => {
             className={"editor-container w-[calc(100%-50px)] h-[calc(100vh-35px)] bg-[#FFF1E9] rounded-tr-[15px] rtl after:inline-block after:border-0 after:z-[-1] after:w-[30px] after:h-[30px] float-end after:bg-[#ffdecc] after:absolute relative after:top-0 after:right-0"}>
             <div id="editor" className={"rounded-tr-[15px] overflow-auto h-[calc(100vh-35px)]"}></div>
         </main>
-        <Draggable>
-            <div className={"window"}></div>
-        </Draggable>
+        <Rnd
+            size={{ width: size.width, height: size.height }}
+            position={{ x: position.x, y: position.y }}
+            onDragStop={(e, d) => setPosition({ x: d.x, y: d.y })}
+            onResizeStop={(e, direction, ref, delta, position) => {
+                setSize({
+                    width: ref.offsetWidth,
+                    height: ref.offsetHeight,
+                });
+                setPosition(position);
+            }}>
+            <div className={"window"}>
+                <i className={"fas fa-school"}></i>
+            </div>
+        </Rnd>
     </>);
 };
 
