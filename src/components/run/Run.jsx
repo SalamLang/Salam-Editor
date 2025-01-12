fdb338c1831c8aa6b617c2001b19d41e0afd6b8d
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-12 21:16:58 +0330

    feat(system): add auto get localstorage system

--- src/components/run/Run.jsx
+++ src/components/run/Run.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+
 const Run = () => {
+  const [code, setCode] = useState(localStorage?.getItem("code"));
+
+  useEffect(() => {
+    setInterval(() => {
+      if (localStorage?.getItem("code") !== code) {
+        setCode(localStorage?.getItem("code"));
+      }
+    }, 1000);
+  }, []);
+
   return <></>;
 };
 
