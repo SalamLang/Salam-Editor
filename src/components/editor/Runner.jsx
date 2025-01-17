01fa4bf8e4eff400be7b1e93186c7b73cbb4ef23
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-17 04:02:14 +0330

    feat(level2): add level 2 button

--- src/components/editor/Runner.jsx
+++ src/components/editor/Runner.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Tooltip } from "react-tooltip";
 import { useNavigate } from "react-router-dom";
 
-const Runner = () => {
+const Runner = (callback) => {
   const [hidden, setHidden] = useState(true);
 
   const navigate = useNavigate();
@@ -38,7 +38,9 @@ const Runner = () => {
       window.open("/run", "_blank");
     };
 
-    const level2 = () => {};
+    const level2 = () => {
+      callback();
+    };
 
     const level3 = () => {};
 
@@ -58,7 +60,7 @@ const Runner = () => {
       <div
         className={
           (hidden && "opacity-0 ") +
-          " shadow-orange fixed top-[75px] transition-all duration-1000 left-[70px] z-[998] bg-white w-fit h-[40px] rounded-[10px] border flex justify-end items-center px-3"
+          " shadow-orange fixed top-[75px] transition-all duration-1000 left-[70px] z-[998] bg-white w-fit h-[40px] rounded-[10px] border flex justify-end items-center px-3 gap-1 flex-row-reverse"
         }
       >
         <button
@@ -89,11 +91,32 @@ const Runner = () => {
             </g>
           </svg>
         </button>
-        <Tooltip
-          id="level-1"
-          className={"!text-[12px] !rounded-[10px]"}
-          delayShow={800}
-        />
+        <button
+          className={"cursor-pointer "}
+          data-tooltip-id="level-2"
+          data-tooltip-content="اجرا به صورت دوگانه"
+          onClick={() => {
+            runOutput(2);
+          }}
+        >
+          <svg
+            width="42"
+            height="36"
+            viewBox="-8 -7 42 36"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="fill-sky-100 stroke-sky-500 -mr-[7px]"
+          >
+            <path
+              d="M12 3h9a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-9"
+              fill="none"
+            ></path>
+            <path d="M3 17V5a2 2 0 0 1 2-2h7a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H5a2 2 0 0 1-2-2Z"></path>
+          </svg>
+        </button>
+        <Tooltip id="level-1" className={"!text-[12px] !rounded-[10px]"} />
+        <Tooltip id="level-2" className={"!text-[12px] !rounded-[10px]"} />
       </div>
     </>
   );
