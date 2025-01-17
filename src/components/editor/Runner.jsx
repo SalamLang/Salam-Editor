d7f097cd17af546088189f17c8966d42ccf92b09
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-17 15:02:05 +0330

    feat(runner): add level 3

--- src/components/editor/Runner.jsx
+++ src/components/editor/Runner.jsx
@@ -72,29 +72,25 @@ const Runner = ({ callback }) => {
           }}
         >
           <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            className={"w-[20px]"}
-            xmlns="http://www.w3.org/2000/svg"
+            width="42"
+            height="36"
+            viewBox="-8 -7 42 36"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="fill-sky-100 stroke-sky-500 -ml-[6px] -mr-[6px]"
           >
-            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              <path
-                d="M21.4086 9.35258C23.5305 10.5065 23.5305 13.4935 21.4086 14.6474L8.59662 21.6145C6.53435 22.736 4 21.2763 4 18.9671L4 5.0329C4 2.72368 6.53435 1.26402 8.59661 2.38548L21.4086 9.35258Z"
-                fill="#276EF6"
-              ></path>
-            </g>
+            <path
+              d="M15 19h6a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H4a1 1 0 0 0-1 1"
+              fill="none"
+            ></path>
+            <path d="M12 17V9a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h5a2 2 0 0 0 2-2Z"></path>
           </svg>
         </button>
         <button
           className={"cursor-pointer "}
           data-tooltip-id="level-2"
-          data-tooltip-content="اجرا به صورت دوگانه"
+          data-tooltip-content="اجرا به صورت عمودی"
           onClick={() => {
             runOutput(2);
           }}
@@ -115,8 +111,33 @@ const Runner = ({ callback }) => {
             <path d="M3 17V5a2 2 0 0 1 2-2h7a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H5a2 2 0 0 1-2-2Z"></path>
           </svg>
         </button>
+        <button
+          className={"cursor-pointer "}
+          data-tooltip-id="level-3"
+          data-tooltip-content="اجرا به صورت افقی"
+          onClick={() => {
+            runOutput(3);
+          }}
+        >
+          <svg
+            width="42"
+            height="36"
+            viewBox="-8 -7 42 36"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="fill-sky-100 stroke-sky-500 -mr-[7px]"
+          >
+            <path d="M23 11V3H3v8h20Z" strokeWidth="0"></path>
+            <path
+              d="M23 17V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2ZM22 11H4"
+              fill="none"
+            ></path>
+          </svg>
+        </button>
         <Tooltip id="level-1" className={"!text-[12px] !rounded-[10px]"} />
         <Tooltip id="level-2" className={"!text-[12px] !rounded-[10px]"} />
+        <Tooltip id="level-3" className={"!text-[12px] !rounded-[10px]"} />
       </div>
     </>
   );
