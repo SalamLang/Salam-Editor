533f5c5726bc03b080d4f75da5c93d7c7294cf7f
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-19 20:41:38 +0330

    feat(axios): add axios config

---
+++ src/components/config/AxiosConfig.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+const instance = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/v1",
+});
+
+instance.interceptors.response.use(null, (error) => {
+  return true;
+});
+
+export const client = {
+  get: instance.get,
+  post: instance.post,
+  put: instance.put,
+  delete: instance.delete,
+};
+
+export const setToken = () => {
+  const token = localStorage?.getItem("token");
+
+  if (token !== undefined) {
+    instance.defaults.headers.common["authorization"] = "Bearer " + token;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const checkToken = async () => {
+  let statue = setToken();
+
+  if (statue) {
+    let result = await client.post("/token");
+    if (result.data !== undefined) {
+      return result.data.success === true;
+    } else {
+      return false;
+    }
+  } else {
+    return statue;
+  }
+};
