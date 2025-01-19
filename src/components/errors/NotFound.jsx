680965a949a4433d0ed5f5f661a6e4b0796f2bb1
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-19 19:45:15 +0330

    feat(404): add 404 page style

--- src/components/errors/NotFound.jsx
+++ src/components/errors/NotFound.jsx
@@ -1,13 +1,28 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
     <>
-      <h1
-        className={
-          "text-[200px] font-black text-[#FF5C00] text-center shadow-1"
-        }
-      >
-        404
-      </h1>
+      <div className={"flex h-[100vh] flex-col items-center justify-center"}>
+        <h1
+          className={
+            "text-[200px] -mb-8 font-black text-[#FF5C00] text-center text-shadow"
+          }
+        >
+          404
+        </h1>
+        <p className={"text-center text-[#FF5C00] text-[20px]"}>
+          صفحه مورد نظر یافت نشد!
+        </p>
+        <Link
+          to={"/"}
+          className={
+            "bg-[#FF5C00] block text-center w-[150px] mt-8 mx-auto text-white p-3 px-4 rounded-full"
+          }
+        >
+          رفتن به ادیتور
+        </Link>
+      </div>
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{scripts,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      boxShadow: {
-        1: "0 0 0 0 #FF5C00",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
