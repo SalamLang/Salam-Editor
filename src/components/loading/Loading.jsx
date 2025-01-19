5ed6efc652826901ac86bdb8a819a82358994b49
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-19 20:03:08 +0330

    feat(loading): add loading

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,5 +1,16 @@
 const Loading = () => {
-  return <>loading</>;
+  return (
+    <>
+      <div
+        className={
+          "w-full h-[100vh] flex justify-center items-center flex-col gap-4"
+        }
+      >
+        <img src={"/images/favicon.svg"} alt="" className={"w-[80px]"} />
+        <p className={"text-[#FF5C00] text-[20px]"}>منتظر بمانید...</p>
+      </div>
+    </>
+  );
 };
 
 export default Loading;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import EditorLayout from "./layouts/EditorLayout.jsx";
+import { lazy } from "react";
+// eslint-disable-next-line react-refresh/only-export-components
+const EditorLayout = lazy(() => import("./layouts/EditorLayout"));
+// eslint-disable-next-line react-refresh/only-export-components
+const RunLayout = lazy(() => import("./layouts/RunLayout"));
 import RouteChangeHandler from "./components/nprogress/RouteChangeHandler.jsx";
 import FireWork from "./components/FireWork.jsx";
 import { ToastContainer } from "react-toastify";
-import RunLayout from "./layouts/RunLayout.jsx";
 import SalamConfig from "./components/config/SalamConfig.jsx";
 import NotFound from "./components/errors/NotFound.jsx";
 import { Suspense } from "react";
