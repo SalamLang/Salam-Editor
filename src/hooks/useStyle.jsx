c80e589a3757bf2fce99c1230cf12bf8bc4b44b7
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-19 23:54:15 +0330

    feat(usestyle): add use style hook

---
+++ src/hooks/useStyle.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { client } from "../components/config/AxiosConfig.js";
+
+const useStyle = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      return await client.get(
+        "https://salamlang.github.io/Salam/config/json/layout/attribute/style/type.json",
+      );
+    };
+
+    fetch().then((d) => {
+      let data = d.data.items;
+
+      let result = data.map((item, index) => {
+        return item.text?.fa.map((item2, index) => {
+          return {
+            type: "text",
+            label: item.text?.fa[index],
+            apply: item.text?.fa[index] + "=«»",
+          };
+        });
+      });
+
+      setData(result);
+    });
+  }, []);
+
+  return data?.flat();
+};
+
+export default useStyle;
