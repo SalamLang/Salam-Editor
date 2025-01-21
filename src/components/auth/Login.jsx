5675f009230237541e377213f677a0ca5c8708bf
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-21 21:49:47 +0330

    feat(setinterval): add timer to count

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,7 +1,22 @@
 import { useEffect, useState } from "react";
+import { cursorCharBackwardLogical } from "@codemirror/commands";
 
 const Login = () => {
   const [line, setLine] = useState(-200);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLine((prevLine) => {
+        if (prevLine < 100) {
+          console.log(prevLine);
+          return prevLine + 1;
+        } else {
+          clearInterval(interval);
+          return prevLine;
+        }
+      });
+    }, 100);
+  }, []);
 
   return (
     <>
@@ -13,7 +28,7 @@ const Login = () => {
         >
           <div className="basis-1/2 bg-gradient-to-b from-[#ff9d00] to-[#ff5c00]">
             <hr
-              className={`bg-gradient-to-r from-[#fff] from-[${line}%] to-transparent opacity-100 border-0 w-[300px] h-[2px] rounded mt-10 mr-10 -rotate-3`}
+              className={`bg-gradient-to-r from-[#fff] transition-all duration-300 from-[${line}%] to-transparent opacity-100 border-0 w-[300px] h-[2px] rounded mt-10 mr-10 -rotate-3`}
             />
           </div>
           <div className="basis-1/2"></div>
