fdd435b55ffd1bdaded7aac8bf577fcf06e3ca70
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-21 21:39:03 +0330

    feat(add): add line from vatiable

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 
 const Login = () => {
+  const [line, setLine] = useState(-200);
+
   return (
     <>
       <div className="w-full h-[100vh] flex justify-center items-center">
@@ -11,7 +13,7 @@ const Login = () => {
         >
           <div className="basis-1/2 bg-gradient-to-b from-[#ff9d00] to-[#ff5c00]">
             <hr
-              className={`bg-gradient-to-r from-[#fff] from-[${line}] to-transparent opacity-100 border-0 w-[300px] h-[2px] rounded mt-10 mr-10 -rotate-3`}
+              className={`bg-gradient-to-r from-[#fff] from-[${line}%] to-transparent opacity-100 border-0 w-[300px] h-[2px] rounded mt-10 mr-10 -rotate-3`}
             />
           </div>
           <div className="basis-1/2"></div>
