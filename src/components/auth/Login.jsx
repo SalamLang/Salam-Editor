71b7a70b80099353e416543677c667bee826262c
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-21 20:53:30 +0330

    feat(gradient): add bg

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
             "w-full max-w-[900px] h-full max-h-[500px] bg-white rounded-[15px] border shadow-lg flex justify-center overflow-hidden"
           }
         >
-          <div className="basis-1/2"></div>
+          <div className="basis-1/2 bg-gradient-to-b from-[#ff9d00] to-[#ff5c00]"></div>
           <div className="basis-1/2"></div>
         </div>
       </div>
