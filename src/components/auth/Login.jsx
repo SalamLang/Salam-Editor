232bac554c951b3592cc036879b4cd2778dee157
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-21 21:37:44 +0330

    feat(line)

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -9,7 +9,11 @@ const Login = () => {
             "w-full max-w-[900px] h-full max-h-[500px] bg-white rounded-[15px] border shadow-lg flex justify-center overflow-hidden"
           }
         >
-          <div className="basis-1/2 bg-gradient-to-b from-[#ff9d00] to-[#ff5c00]"></div>
+          <div className="basis-1/2 bg-gradient-to-b from-[#ff9d00] to-[#ff5c00]">
+            <hr
+              className={`bg-gradient-to-r from-[#fff] from-[${line}] to-transparent opacity-100 border-0 w-[300px] h-[2px] rounded mt-10 mr-10 -rotate-3`}
+            />
+          </div>
           <div className="basis-1/2"></div>
         </div>
       </div>
