d1a52ccb19a6a491fd9945a04d6943a8debf4fe6
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-21 20:33:44 +0330

    feat(login): add login box

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,5 +1,15 @@
 const Login = () => {
-  return <>hi</>;
+  return (
+    <>
+      <div className="w-full h-[100vh] flex justify-center items-center">
+        <div
+          className={
+            "w-full max-w-[900px] h-full max-h-[500px] bg-white rounded-[15px] border shadow-lg flex justify-center items-center"
+          }
+        ></div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
