25c52ff64ce2834de4231bb4bd4aabaa1669bb75
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-28 18:57:28 +0330

    feat(login): add login layout

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -1,23 +1,36 @@
 import Login from "../components/auth/Login.jsx";
 import Verify from "../components/auth/Verify.jsx";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import LoginContext from "../context/LoginContext.jsx";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const EditorLayout = () => {
   const [isVerifying, setIsVerifying] = useState(false);
   const [mobile, setMobile] = useState(null);
+  const { login } = useContext(LoginContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (login) {
+      navigate("/");
+    }
+  }, [login, navigate]);
 
   return (
     <>
-      {isVerifying === false && (
-        <Login
-          callback={(mobile) => {
-            setIsVerifying(true);
-            setMobile(mobile);
-          }}
-        />
+      {login === false && (
+        <>
+          {isVerifying === false && (
+            <Login
+              callback={(mobile) => {
+                setIsVerifying(true);
+                setMobile(mobile);
+              }}
+            />
+          )}
+          {isVerifying && <Verify mobile={mobile} />}
+        </>
       )}
-
-      {isVerifying && <Verify mobile={mobile} />}
     </>
   );
 };
