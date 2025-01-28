1e08499d63e04919447bdb028ab286a423c0a354
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-28 22:26:19 +0330

    feat(share): add share feature

--- src/layouts/EditorLayout.jsx
+++ src/layouts/EditorLayout.jsx
@@ -4,18 +4,27 @@ import Editor from "../components/editor/Editor.jsx";
 import Logo from "../components/editor/Logo.jsx";
 import { useParams } from "react-router-dom";
 import GetCodeService from "../services/GetCodeService.js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Loading from "../components/loading/Loading.jsx";
 const EditorLayout = () => {
   const params = useParams();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (params.id !== undefined) {
-      GetCodeService(params.id).then((result) => {});
+      GetCodeService(params.id).then((result) => {
+        localStorage.setItem("code", result.data.code);
+        document.title = result.data.title;
+        setLoading(false);
+      });
+    } else {
+      setLoading(false);
     }
   }, [params]);
 
   return (
     <>
+      {loading && <Loading />}
       <Header />
       <Logo />
       <Side />
