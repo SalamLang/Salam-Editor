38d778de52ca945fb3607b904478702955c33245
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-28 18:57:52 +0330

    refactor(clean): clean code with pint

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -2,7 +2,7 @@ import Login from "../components/auth/Login.jsx";
 import Verify from "../components/auth/Verify.jsx";
 import { useContext, useEffect, useState } from "react";
 import LoginContext from "../context/LoginContext.jsx";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const EditorLayout = () => {
   const [isVerifying, setIsVerifying] = useState(false);
