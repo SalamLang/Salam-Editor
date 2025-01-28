ab092fccccecc7ee3637556826991fd14b30aa30
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-28 18:25:06 +0330

    feat(login): add login context

---
+++ src/provider/LoginProvider.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import LoginContext from "../context/LoginContext.jsx";
+import { checkToken } from "../components/config/AxiosConfig.js";
+import Loading from "../components/loading/Loading.jsx";
+
+// eslint-disable-next-line react/prop-types
+const LoginProvider = ({ children }) => {
+  const [login, setLogin] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    checkToken().then((result) => {
+      setLogin(result);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <LoginContext.Provider value={{ login, setLogin }}>
+      {loading && <Loading />}
+      {children}
+    </LoginContext.Provider>
+  );
+};
+
+export default LoginProvider;
