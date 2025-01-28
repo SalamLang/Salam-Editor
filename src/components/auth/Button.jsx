1af85c259cab1063f4168118609428e107a42e6a
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-28 15:34:07 +0330

    feat(loading): handle loading

--- src/components/auth/Button.jsx
+++ src/components/auth/Button.jsx
@@ -11,7 +11,73 @@ const Label = ({ type, className, children, onClick, disabled }) => {
           className
         }
       >
-        {children}
+        {disabled === true ? (
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={"w-[40px] mx-auto animate-spin"}
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M4.97498 12H7.89998"
+                stroke="#fff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              ></path>
+              <path
+                d="M11.8 5V8"
+                stroke="#fff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              ></path>
+              <path
+                d="M18.625 12H15.7"
+                stroke="#fff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              ></path>
+              <path
+                d="M11.8 19V16"
+                stroke="#fff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              ></path>
+              <path
+                d="M6.97374 16.95L9.04203 14.8287"
+                stroke="#fff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              ></path>
+              <path
+                d="M6.97374 7.05001L9.04203 9.17133"
+                stroke="#fff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              ></path>
+              <path
+                d="M16.6262 7.05001L14.5579 9.17133"
+                stroke="#fff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              ></path>
+              <path
+                d="M16.6262 16.95L14.5579 14.8287"
+                stroke="#fff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+              ></path>
+            </g>
+          </svg>
+        ) : (
+          children
+        )}
       </button>
     </>
   );
