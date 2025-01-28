a64ac28e5a54726878a696dbd8fd9c16c91737f3
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-28 22:41:45 +0330

    feat(title): chnage title

--- src/layouts/EditorLayout.jsx
+++ src/layouts/EditorLayout.jsx
@@ -2,25 +2,26 @@ import Header from "../components/header/Header.jsx";
 import Side from "../components/sidebar/Side.jsx";
 import Editor from "../components/editor/Editor.jsx";
 import Logo from "../components/editor/Logo.jsx";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import GetCodeService from "../services/GetCodeService.js";
 import { useEffect, useState } from "react";
 import Loading from "../components/loading/Loading.jsx";
 const EditorLayout = () => {
   const params = useParams();
   const [loading, setLoading] = useState(true);
+  const location = useLocation();
 
   useEffect(() => {
     if (params.id !== undefined) {
       GetCodeService(params.id).then((result) => {
-        localStorage.setItem("code", result.data.code);
-        document.title = result.data.title;
+        localStorage.setItem("code", result?.data?.code ?? "");
+        document.title = result?.data?.title ?? "چی میزنی حاجی";
         setLoading(false);
       });
     } else {
       setLoading(false);
     }
-  }, [params]);
+  }, [params, location]);
 
   return (
     <>
