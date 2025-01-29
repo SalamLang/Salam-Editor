e2a70b6e8641817547051e5621ff91052a11fd22
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-29 09:59:53 +0330

    feat(map): add map on tabs

--- src/components/editor/Logo.jsx
+++ src/components/editor/Logo.jsx
@@ -3,14 +3,15 @@ import { useContext, useState } from "react";
 import LoginContext from "../../context/LoginContext.jsx";
 import Modal from "../shared/Modal.jsx";
 import Profile from "../shared/Profile.jsx";
+import Codes from "../shared/Codes.jsx";
 
 const Logo = () => {
   const { login } = useContext(LoginContext);
   const [openProfile, setOpenProfile] = useState(false);
 
   const tabs = [
-    { id: "profile", active: true, element: <Profile /> },
-    { id: "codes", active: false, element: <Codes /> },
+    { id: "profile", label: "پروفایل", active: true, element: <Profile /> },
+    { id: "codes", label: "کد های من", active: false, element: <Codes /> },
   ];
 
   return (
@@ -46,11 +47,17 @@ const Logo = () => {
             callback={() => {
               setOpenProfile(false);
             }}
-            className={"!w-[750px] !h-[450px]"}
+            className={"!w-[750px] !h-[450px] p-3"}
           >
             <div className="w-full h-full flex">
-              <div className={"side basis-2/12"}></div>
-              <div className={"side bg-yellow-400 basis-10/12"}></div>
+              <div className={"side basis-2/12"}>
+                {tabs.map((tab) => (
+                  <button key={tab.id} className={"block"}>
+                    {tab.label}
+                  </button>
+                ))}
+              </div>
+              <div className={"basis-10/12"}></div>
             </div>
           </Modal>
         </>
