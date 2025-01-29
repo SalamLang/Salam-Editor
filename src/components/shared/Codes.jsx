8f77535074c7254a00a67ba622ca8d510caace44
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-29 17:28:42 +0330

    feat(codes): add share to codes

--- src/components/shared/Codes.jsx
+++ src/components/shared/Codes.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import GetCodeService from "../../services/GetCodeService.js";
 import GetMyCodeService from "../../services/GetMyCodeService.js";
 import { OrbitProgress } from "react-loading-indicators";
 import Svg from "./Svg.jsx";
 import { Tooltip } from "react-tooltip";
+import Modal from "./Modal.jsx";
+import { toast } from "react-hot-toast";
 
 const Codes = () => {
   const location = useLocation();
   const [loading, setLoading] = useState(true);
   const [codes, setCodes] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [id, setId] = useState(null);
 
   useEffect(() => {
     GetMyCodeService().then((result) => {
@@ -18,7 +21,20 @@ const Codes = () => {
     });
   }, [location]);
 
-  const share = () => {};
+  const copyAddress = () => {
+    navigator.clipboard
+      .writeText("https://editor.salamlang.ir/" + id)
+      .then(() => {
+        toast.success("کپی شد!", {
+          position: "bottom-center",
+        });
+      });
+  };
+
+  const share = (id) => {
+    setOpenModal(true);
+    setId(id);
+  };
 
   const open_new_tab = (id) => {
     window.open("/" + id, "_blank");
@@ -93,6 +109,54 @@ const Codes = () => {
           </div>
         )}
       </div>
+      <Modal
+        show={openModal}
+        className={"!w-[360px] h-auto pb-8"}
+        callback={() => {
+          setOpenModal(false);
+        }}
+      >
+        <img
+          src="/images/share.png"
+          className={"w-[130px] mx-auto mt-[30px]"}
+          alt=""
+        />
+        <h1 className={"text-center text-[20px] mt-3 font-bold"}>
+          این لینک رو میتونی استفاده کنی
+        </h1>
+        <p
+          className={
+            "text-center text-gray-400 text-[14px] w-[200px] mt-3 mx-auto"
+          }
+        >
+          برای به اشتراک گذاشتن لینک زیر به دوستان خود ارسال کنید
+        </p>
+        <div
+          className={
+            "w-[85%] h-[45px] p-[3px] mt-6 rounded-[15px] border mx-auto flex justify-center items-center"
+          }
+        >
+          <div
+            className={
+              "w-full h-full rounded-[10px] bg-[#e6efff] flex items-center px-3 cursor-pointer justify-between flex-row-reverse"
+            }
+            onClick={() => {
+              copyAddress();
+            }}
+            data-tooltip-id="copy"
+            data-tooltip-content="کپی"
+          >
+            <span className={"text-[#2b71f6]"} dir={"ltr"}>
+              editor.salamlang.ir/{id?.substring(0, 6)}...
+            </span>
+            <Svg name={"share"} theme={"#276EF6"} />
+          </div>
+        </div>
+      </Modal>
+      <Tooltip
+        id={"copy"}
+        className={"z-[1005] !text-[14px] !rounded-[10px]"}
+      />
       <Tooltip id={"share"} className={"!text-[13px] !rounded-[10px]"} />
       <Tooltip id={"open_new_tab"} className={"!text-[13px] !rounded-[10px]"} />
     </>
