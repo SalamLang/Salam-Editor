e2adb440f7af738aa09d85942c0596a0f337622f
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-29 19:19:23 +0330

    feat(headaer): add header

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/admin/Header.jsx";
 
 const AdminLayout = () => {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
