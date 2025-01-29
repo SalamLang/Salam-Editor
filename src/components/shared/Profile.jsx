0a879ab4ec33c96d9968895ae3b485dee6548978
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-29 11:04:52 +0330

    feat(logout): add logout btn

--- src/components/shared/Profile.jsx
+++ src/components/shared/Profile.jsx
@@ -1,7 +1,20 @@
+import Button from "../auth/Button.jsx";
+import Svg from "./Svg.jsx";
+
 const Profile = () => {
   return (
     <>
-      <div className="w-full h-full bg-black"></div>
+      <div className="w-full h-full p-3">
+        <Button
+          type={"button"}
+          className={
+            "border-2 bg-transparent !text-red-600 border-red-600 flex justify-center items-center gap-2"
+          }
+        >
+          <Svg name={"alert"} theme={"#E7000B"} />
+          خروج از حساب
+        </Button>
+      </div>
     </>
   );
 };
