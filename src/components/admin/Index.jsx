e10e4a45748169be53091a036ba4e018a2db5739
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-29 21:03:36 +0330

    feat(code): add code svg

--- src/components/admin/Index.jsx
+++ src/components/admin/Index.jsx
@@ -4,6 +4,7 @@ import GetMyCodeService from "../../services/GetMyCodeService.js";
 import { OrbitProgress } from "react-loading-indicators";
 import GetAdminIndexService from "../../services/GetAdminIndexService.js";
 import Line from "../shared/Line.jsx";
+import Svg from "../shared/Svg.jsx";
 
 const Index = () => {
   const location = useLocation();
@@ -38,7 +39,21 @@ const Index = () => {
         </>
       )}
 
-      {loading === false && <></>}
+      {loading === false && (
+        <>
+          <div className={"flex justify-center items-center gap-[10px]"}>
+            <div
+              className={
+                "bg-orange-500 basis-1/3 relative overflow-hidden p-4 rounded-[15px] text-white"
+              }
+            >
+              <h2>تعداد کل کد ها</h2>
+              <h3 className={"mt-2 mb-0"}>{data?.codes?.count}</h3>
+              <Svg name={"code"} />
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
