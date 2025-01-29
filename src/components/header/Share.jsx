ac1f81c0b94ad03e9596c9ec39c534c889c5a5fb
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-29 08:19:06 +0330

    feat(modal): add share moda

--- src/components/header/Share.jsx
+++ src/components/header/Share.jsx
@@ -55,10 +55,65 @@ const Share = () => {
       />
       <Modal
         show={openModal}
+        className={"!w-[330px]"}
         callback={() => {
           setOpenModal(false);
         }}
-      ></Modal>
+      >
+        <img
+          src="/images/share.png"
+          className={"w-[100px] mx-auto mt-[30px]"}
+          alt=""
+        />
+        <h1 className={"text-center text-[19px] mt-3 font-bold"}>
+          این لینک رو میتونی استفاده کنی
+        </h1>
+        <p
+          className={
+            "text-center text-gray-400 text-[13px] w-[200px] mt-3 mx-auto"
+          }
+        >
+          برای به اشتراک گذاشتن لینک زیر به دوستان خود ارسال کنید
+        </p>
+        <div
+          className={
+            "w-[80%] h-[45px] p-[3px] mt-4 rounded-[15px] border mx-auto flex justify-center items-center"
+          }
+        >
+          <div
+            className={
+              "w-full h-full rounded-[10px] bg-[#e6efff] flex items-center px-3 cursor-pointer justify-between flex-row-reverse"
+            }
+          >
+            <span className={"text-[#2b71f6]"}>editor.salamlang.ir</span>
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className={"w-[25px]"}
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  d="M6 11C6 8.17157 6 6.75736 6.87868 5.87868C7.75736 5 9.17157 5 12 5H15C17.8284 5 19.2426 5 20.1213 5.87868C21 6.75736 21 8.17157 21 11V16C21 18.8284 21 20.2426 20.1213 21.1213C19.2426 22 17.8284 22 15 22H12C9.17157 22 7.75736 22 6.87868 21.1213C6 20.2426 6 18.8284 6 16V11Z"
+                  stroke="#2b71f6"
+                  strokeWidth="1.5"
+                ></path>
+                <path
+                  d="M6 19C4.34315 19 3 17.6569 3 16V10C3 6.22876 3 4.34315 4.17157 3.17157C5.34315 2 7.22876 2 11 2H15C16.6569 2 18 3.34315 18 5"
+                  stroke="#2b71f6"
+                  strokeWidth="1.5"
+                ></path>
+              </g>
+            </svg>
+          </div>
+        </div>
+      </Modal>
     </>
   );
 };
