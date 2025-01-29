49919f7b911d53e55fab4565fbb9bdc547a87d4a
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-29 11:06:47 +0330

    feat(line): add line component

--- src/components/shared/Profile.jsx
+++ src/components/shared/Profile.jsx
@@ -5,6 +5,12 @@ const Profile = () => {
   return (
     <>
       <div className="w-full h-full p-3">
+        <div className="flex items-center gap-[10px] my-6">
+          <span className="text-nowrap font-bold text-black text-[18px] pr-4 relative before:block before:w-[12px] before:h-[12px] before:bg-[#F8981C] before:rounded-full before:absolute before:top-1/2 before:-translate-y-1/2 before:right-0">
+            اطلاعات وارد کننده
+          </span>
+          <hr className="opacity-100 border-0 h-[1px] w-full bg-gray-300 " />
+        </div>
         <Button
           type={"button"}
           className={
