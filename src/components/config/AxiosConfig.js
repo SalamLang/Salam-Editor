3e750cfa8c2daf97bda7c0f6934f51393ef7d07a
mohamadreza <mohamadreza1388.org@gmail.com>
2025-01-31 13:44:45 +0330

    feat(base): chnage base address

--- src/components/config/AxiosConfig.js
+++ src/components/config/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL: "https://api.salamlang.ir/api/v1",
 });
 
 instance.interceptors.response.use(null, (error) => {
