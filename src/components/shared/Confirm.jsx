f1dad83bcf5d1b53cf2df4089ca44d0d1bbfa734
mohamadreza <mohamadreza1388.org@gmail.com>
2025-02-02 23:51:46 +0330

    feat(confirm)

--- src/components/shared/Confirm.jsx
+++ src/components/shared/Confirm.jsx
@@ -1,20 +1,3 @@
-import { useEffect, useState } from "react";
-import Modal from "./Modal.jsx";
-
-const Confirm = ({ title }) => {
-  const [confirmed, setConfirmed] = useState(null);
-
-  useEffect(() => {}, []);
+const Confirm = ({ accept, reject, show, callback }) => {
+  return <></>;
 };
-
-const ConfirmLayout = () => {
-  return (
-    <>
-      <Modal show={true} className={"w-[400px] h-[350px]"}></Modal>
-    </>
-  );
-};
-
-export { ConfirmLayout };
-
-export default Confirm;
