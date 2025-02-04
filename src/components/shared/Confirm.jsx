8f3ab92f2487334814481f514ec275f2b5016c34
mohamadreza <mohamadreza1388.org@gmail.com>
2025-02-04 15:32:50 +0330

    feat(buuton): add btn styles

--- src/components/shared/Confirm.jsx
+++ src/components/shared/Confirm.jsx
@@ -1,5 +1,6 @@
 import Modal from "./Modal.jsx";
 import { useEffect, useState } from "react";
+import Button from "../auth/Button.jsx";
 
 const Confirm = ({ accept, reject, show, callback }) => {
   const [open, setOpen] = useState(false);
@@ -16,13 +17,37 @@ const Confirm = ({ accept, reject, show, callback }) => {
           callback();
         }}
         fixed={false}
-        className={"w-[450px] h-[250px]"}
+        className={"w-[400px] !h-auto"}
       >
-        <h1
-          className={"text-red-600 text-[20px] font-bold text-center mt-[20px]"}
-        >
-          از کار خود مطمئنید؟
+        <img
+          src="/images/confirm.png"
+          alt="confirm"
+          className={"block mx-auto w-[110px]"}
+        />
+        <h1 className={"text-red-600 text-[18px] font-bold text-center -mt-2"}>
+          از کار خودت مطمئنی؟
         </h1>
+
+        <div
+          className={"flex justify-center items-center gap-[10px] px-4 pb-4"}
+        >
+          <Button
+            theme={"#E7000B"}
+            className={
+              "border-2 bg-transparent !text-red-600 hover:shadow-xl transition-all duration-300 !shadow-red-600/30 border-red-600 flex justify-center items-center gap-2"
+            }
+          >
+            اره مطمئنم
+          </Button>
+          <Button
+            theme={"#bababa"}
+            className={
+              "border-2 bg-transparent !text-gray-400/95 !border-gray-400/95 flex justify-center items-center gap-2 hover:shadow-xl transition-all duration-300 !shadow-gray-600/30"
+            }
+          >
+            نه ولش کن!
+          </Button>
+        </div>
       </Modal>
     </>
   );
