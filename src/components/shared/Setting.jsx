6383d15b8c047d0a65081f23937ae46c32667826
mohamadreza <mohamadreza1388.org@gmail.com>
2025-02-06 00:36:10 +0330

    feat(handle): handle save theme to storage

--- src/components/shared/Setting.jsx
+++ src/components/shared/Setting.jsx
@@ -24,8 +24,10 @@ const Setting = ({ show, callback }) => {
           onClick={() => {
             if (document.documentElement.classList.contains("dark")) {
               document.documentElement.classList.remove("dark");
+              localStorage.setItem("theme", "light");
             } else {
               document.documentElement.classList.add("dark");
+              localStorage.setItem("theme", "dark");
             }
           }}
         >
