3076bc006a24c845968d82211c1cd6f535a9b75f
mohamadreza <mohamadreza1388.org@gmail.com>
2025-02-12 18:09:05 +0330

    feat(attr): inport attr from json

--- src/hooks/useAttr.jsx
+++ src/hooks/useAttr.jsx
@@ -1,38 +1,31 @@
 import { useEffect, useState } from "react";
-import { client } from "../components/config/AxiosConfig.js";
 
 const useAttr = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    const fetch = async () => {
-      return await client.get(
-        "https://salamlang.github.io/Salam/config/json/layout/attribute/type.json",
-      );
-    };
-
-    fetch().then((d) => {
-      let data = d.data.items;
-
-      let result = data.map((item, index) => {
-        if (index !== 0) {
-          return item.text?.fa.map((item2, index) => {
+    fetch("/src/data/attr.json").then((response) => {
+      response.json().then((data) => {
+        let result = data.items.map((item, index) => {
+          if (index !== 0) {
+            return item.text?.fa.map((item2, index) => {
+              return {
+                type: "keyword",
+                label: item.text?.fa[index],
+                apply: item.text?.fa[index] + "=«»",
+              };
+            });
+          } else {
             return {
               type: "keyword",
-              label: item.text?.fa[index],
-              apply: item.text?.fa[index] + "=«»",
+              label: "ارور",
+              apply: "ارور" + "=«»",
             };
-          });
-        } else {
-          return {
-            type: "keyword",
-            label: "ارور",
-            apply: "ارور" + "=«»",
-          };
-        }
-      });
+          }
+        });
 
-      setData(result);
+        setData(result);
+      });
     });
   }, []);
 
