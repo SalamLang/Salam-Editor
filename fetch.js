6d74c0a6679489b98e8091484b6945ae04b043d7
mohamadreza <mohamadreza1388.org@gmail.com>
2025-02-12 16:44:22 +0330

    feat(urls): add all urls

---
+++ fetch.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import fs from "fs";
+
+let urls = [
+  "https://salamlang.github.io/Salam/config/json/layout/type.json",
+  "https://salamlang.github.io/Salam/config/json/layout/attribute/type.json",
+  "https://salamlang.github.io/Salam/config/json/layout/attribute/style/type.json",
+];
