daaa512ca5ed92691b5f72d59a12645fcf7bacaa
mohamadreza <mohamadreza1388.org@gmail.com>
2025-02-12 18:13:31 +0330

    feat(#81)

--- src/hooks/useStyle.jsx
+++ src/hooks/useStyle.jsx
@@ -1,38 +1,31 @@
 import { useEffect, useState } from "react";
-import { client } from "../components/config/AxiosConfig.js";
 
 const useStyle = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    const fetch = async () => {
-      return await client.get(
-        "https://salamlang.github.io/Salam/config/json/layout/attribute/style/type.json",
-      );
-    };
-
-    fetch().then((d) => {
-      let data = d.data.items;
-
-      let result = data.map((item, index) => {
-        if (index !== 258) {
-          return item.text?.fa.map((item2, index) => {
+    fetch("/src/data/style.json").then((response) => {
+      response.json().then((data) => {
+        let result = data.items.map((item, index) => {
+          if (index !== 258) {
+            return item.text?.fa.map((item2, index) => {
+              return {
+                type: "text",
+                label: item.text?.fa[index],
+                apply: item.text?.fa[index] + "=«»",
+              };
+            });
+          } else {
             return {
               type: "text",
-              label: item.text?.fa[index],
-              apply: item.text?.fa[index] + "=«»",
+              label: "ارور",
+              apply: "ارور" + "=«»",
             };
-          });
-        } else {
-          return {
-            type: "text",
-            label: "ارور",
-            apply: "ارور" + "=«»",
-          };
-        }
-      });
+          }
+        });
 
-      setData(result);
+        setData(result);
+      });
     });
   }, []);
 
