eb59a9641e4d62e85a6b4f0c6bf5c1235b9d4318
mohamadreza <mohamadreza1388.org@gmail.com>
2025-02-12 16:59:07 +0330

    feat(fetch): handle fetch data

--- fetch.js
+++ fetch.js
@@ -2,7 +2,28 @@ import axios from "axios";
 import fs from "fs";
 
 let urls = [
-  "https://salamlang.github.io/Salam/config/json/layout/type.json",
-  "https://salamlang.github.io/Salam/config/json/layout/attribute/type.json",
-  "https://salamlang.github.io/Salam/config/json/layout/attribute/style/type.json",
+  {
+    url: "https://salamlang.github.io/Salam/config/json/layout/type.json",
+    fileName: "tags.json",
+  },
+  {
+    url: "https://salamlang.github.io/Salam/config/json/layout/attribute/type.json",
+    fileName: "attr.json",
+  },
+  {
+    url: "https://salamlang.github.io/Salam/config/json/layout/attribute/style/type.json",
+    fileName: "style.json",
+  },
 ];
+
+urls.forEach((item) => {
+  axios.get(item.url).then((d) => {
+    fs.writeFile(
+      "src/data/" + item.fileName,
+      JSON.stringify(d.data, null, 2),
+      (error) => {
+        console.log(error);
+      },
+    );
+  });
+});
